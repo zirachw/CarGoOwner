eba8daf35cc7d5d9c0fd2b02aaa4e2bbca9dc93a
Razi <zirachwidh@gmail.com>
2024-11-29 05:46:02 +0700

    chore: setup PyQt5

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("CarGoOwner")
+        self.setGeometry(700, 300, 500, 500)
+        #self.setWindowIcon(QIcon("profile_pic.jpg"))
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
