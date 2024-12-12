53dba240bc7f2ddeb3f9ec3d4dd5cd7c3adfed5e
Nayaka Ghana Subrata <katou7.ayaka@gmail.com>
2024-12-12 16:19:35 +0700

    fix : pembayaran rental path

--- src/MainUI.py
+++ src/MainUI.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import Qt
 from Mobil.MobilUI import MobilUI
 from Peminjaman.peminjamanUI import PeminjamanUI
 from Pelanggan.PelangganController import PelangganController
-from Notifikasi.NotifikasiController import StatusPengembalian
+from Notifikasi.NotifikasiController import StatusPengembalian, StatusPembayaran
 from Laporan.HistoriPeminjaman import HistoriPEminjamanController
 from Laporan.Pendapatan import PendapatanController
 from Laporan.StatusKetersediaan import StatusKetersediaanController
@@ -42,7 +42,7 @@ class MenuUI(QMainWindow):
         self.pelanggan = PelangganController(schema_path="src/schema.sql")
         # Panggil Pelanggan UI
         self.jadwalpengembalian = StatusPengembalian(schema_path="src/schema.sql")
-        self.pembayaranrental = QWidget()
+        self.pembayaranrental = StatusPembayaran(schema_path="src/schema.sql")
         self.historipeminjamanmobil = HistoriPEminjamanController(schema_path="src/schema.sql")
         self.statusketersediaanmobil = StatusKetersediaanController(schema_path="src/schema.sql")
         self.pendapatan = PendapatanController(schema_path="src/schema.sql")
